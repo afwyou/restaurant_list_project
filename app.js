b824682217b4abc8af786e2b3eb4294f3045ee75
afwyou <afwyousenge@gmail.com>
2021-05-03 08:18:11 +0800

    feat: 初始化-資料庫連線與建立Restaurant Model

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const port = 3000
 const app = express()
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const restaurantList = require('./restaurant.json')
+mongoose.connect('mongodb://localhost/restaurant-list')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 // 告訴 Express 說要設定的 view engine 是 handlebars
@@ -10,8 +12,17 @@ app.set('view engine', 'handlebars')
 //告訴express靜態檔案在哪裡
 app.use(express.static('public'))
 
+//取得資料庫連線狀態
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 
+//路由區
 app.get('/', (req, res) => {
   const restaurants = restaurantList.results
   res.render('index', { restaurant: restaurants })
@@ -31,5 +42,5 @@ app.get('/search', (req, res) => {
 })
 
 app.listen(port, () => {
-
+  console.log('Listen to the http://localhost:3000')
 })
