7152a26baad0a3547189644c3d5a588491cd9803
afwyou <afwyousenge@gmail.com>
2021-05-30 16:22:16 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
@@ -15,6 +16,11 @@ app.use(express.static('public'))
 app.use((bodyParser.urlencoded({ extended: true })))
 app.use(methodOverride('_method'))
 app.use(routes)
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.listen(3000, () => {
   console.log('Listen to the http://localhost:3000')
