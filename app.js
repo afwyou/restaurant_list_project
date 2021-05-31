609f456bc481f65621c61156e355b35b500a69cc
afwyou <afwyousenge@gmail.com>
2021-05-31 21:44:33 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 const routes = require('./routes')
 require('./config/mongoose')
 
@@ -24,12 +25,14 @@ app.use((bodyParser.urlencoded({ extended: true })))
 app.use(methodOverride('_method'))
 //#2
 usePassport(app)
+app.use(flash())
 //#3
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
-  console.log(res.locals.user)
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 //#last
