e45e5cbdeb3e4cf3bd0fa681ca9c11dde3a9ebc2
afwyou <afwyousenge@gmail.com>
2021-06-01 05:26:57 +0800

    feat: add facebook login & dotenv

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
+//production mode 中，process.env 裡通常會自動新增 NODE_ENV，並將值設定成 "production"
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const routes = require('./routes')
 require('./config/mongoose')
 
@@ -28,7 +32,7 @@ usePassport(app)
 app.use(flash())
 //#3
 app.use((req, res, next) => {
-  // 你可以在這裡 console.log(req.user) 等資訊來觀察
+  //  console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
   res.locals.success_msg = req.flash('success_msg')
@@ -37,6 +41,6 @@ app.use((req, res, next) => {
 })
 //#last
 app.use(routes)
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log('Listen to the http://localhost:3000')
 })
--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
 const User = require('../models/user')
 module.exports = app => {
   // 初始化 Passport 模組
@@ -24,6 +25,34 @@ module.exports = app => {
       })
       .catch(err => done(err, false))
   }))
+  //facebook
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  }, (accessToken, refreshToken, profile, done) => {
+
+    //運用fb提供的json資料來完成登入
+    const { name, email } = profile._json
+    User.findOne({ email })
+      .then(user => {
+        if (user) return done(null, user)
+        //36 進位,截切字串的最後8位
+        const randomPassword = Math.random().toString(36).slice(-8)
+        bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(randomPassword, salt))
+          .then(hash => User.create({
+            name,
+            email,
+            password: hash
+          }))
+          .then(user => done(null, user))
+          .catch(err => done(err, false))
+      })
+  }))
+
 
   // 設定序列化與反序列化
   passport.serializeUser((user, done) => {
